bc3a73cad9dec19ff919a9f58809d151b4d101cb
WandoChocolates <ellytitus50@gmail.com>
2021-08-12 14:32:35 +0800

    book detail page success

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-import { bookAuthors } from './utils';
-
 const BookDetail = ({ book }) => {
   const createDescMarkup = description => {
     return { __html: description };
+  };
+
+  const bookAuthors = authors => {
+    if (authors) {
+      if (authors.length <= 2) {
+        authors = authors.join(' and ');
+      } else if (authors.length > 2) {
+        let lastAuthor = ' and ' + authors.slice(-1);
+        authors.pop();
+        authors = authors.join(', ');
+        authors += lastAuthor;
+      }
+      return authors;
+    }
   };
 
   return (
--- src/components/BookDetailPage.js
+++ src/components/BookDetailPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-import BookDetail from './bookDetail';
+import BookDetail from './BookDetail';
 
 const BookDetailPage = ({ match }) => {
   const {
